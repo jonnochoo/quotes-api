792cf5d78a48b927ee7ce0c11307a6de2914ce5a
Jonathon Choo <jonno.choo@gmail.com>
2015-03-10 19:38:09 +1100

    Added CORS support

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var compress = require('compression');
 var express = require('express');
 var db = require('./lib/db');
 var router = require('./routes');
+var cors = require('cors');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3010);
 
+app.use(cors());
 app.use(compress());
 app.use(bodyParser());
 
